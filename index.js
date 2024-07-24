12518ae9632258a0edd3d09ca78b2e5a8e9b4af7
www-data <www-data@s5294121.elf.ict.griffith.edu.au>
2024-07-25 09:49:14 +1000

    changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('I caused a merge conflict.');
+console.log("hello world");
+function addNums(num1, num2)
+{
+    return num1 + num2;
+}
+
+console.log("Hello World");
+console.log(addNums(5, 10));
