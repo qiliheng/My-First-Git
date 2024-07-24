ac28fa1f8a3307ff97617a7068565f65d6fca36a
www-data <www-data@s5294121.elf.ict.griffith.edu.au>
2024-07-24 23:36:29 +1000

    added adding function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+// index.js
+
+console.log("Hello World");
+
+function addNumbers(a, b) {
+  return a + b;
+}
+
+const sum = addNumbers(5, 10);
+console.log("The sum of 5 and 10 is:", sum);
